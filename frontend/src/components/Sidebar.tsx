22a6444e66e0d0545d7f1d70ad12cf4ca2705c62
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2023-09-09 12:30:05 -0700

    Functioning sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,15 +1,19 @@
 import { Menu } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 
 
 function Sidebar(props: {open: boolean}) {
+    
     return (
-        <div className="bg-gradient-to-b from-light-200 to-light-400 shadow-lg">
-            <div className="flex flex-col w-8 h-full">
-                <Menu></Menu>
+        <div className="flex h-full bg-gradient-to-b from-light-200 to-light-400 shadow-xl flex-shrink-0">
+            <div className="flex flex-col flex-shrink-0 items-center w-12 h-full">
+                <IconButton><Menu fontSize="large"></Menu></IconButton>
             </div>
-            <div className={`flex-shrink-0 w-full ${props.open ? 'sm:max-w-xs max-w-screen-sm' : 'max-w-0'} transition-all`}>
-            
+            <div className={`flex-shrink-0 w-screen ${props.open ? 'sm:max-w-xs max-w-screen-sm' : 'max-w-0'} transition-all overflow-hidden`}>
+                <div className="w-screen sm:max-w-xs max-w-screen-sm">
+                    Woah this is a sidebar
+                </div>
             </div>
         </div>
     )
