39f33666fd0260c30fa67fec77f66bb731bc81a0
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2023-09-09 12:53:28 -0700

    Changed sidebar button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,16 @@
-import {  Button, IconButton, Toolbar, Typography } from "@mui/material";
-import { AppBar } from "@material-ui/core";
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Map from "./components/Map";
 import { POST } from "./util/apiHandler";
-import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
-
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-  
 
   return (
     <div className="w-full h-screen overflow-hidden relative">
       
        
       <div className="w-full h-full overflow-hidden relative flex flex-row">
-        <Sidebar open={sidebarOpen}></Sidebar>
+        <Sidebar></Sidebar>
         <div className="w-full h-full overflow-hidden">
           {/* <Button variant="contained" onClick={()=>setSidebarOpen(!sidebarOpen)}>Toggle Sidebar</Button> */}
           {/* <Button onClick={()=>POST("/get_posts")}></Button> */}
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,16 +1,19 @@
 import { Menu } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { useState } from "react";
 
 
 
-function Sidebar(props: {open: boolean}) {
+function Sidebar() {
     
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+
     return (
         <div className="flex h-full bg-gradient-to-b from-light-200 to-light-400 shadow-xl flex-shrink-0">
             <div className="flex flex-col flex-shrink-0 items-center w-12 h-full">
-                <IconButton><Menu fontSize="large"></Menu></IconButton>
+                <IconButton onClick={()=>setSidebarOpen(!sidebarOpen)}><Menu fontSize="large"></Menu></IconButton>
             </div>
-            <div className={`flex-shrink-0 w-screen ${props.open ? 'sm:max-w-xs max-w-screen-sm' : 'max-w-0'} transition-all overflow-hidden`}>
+            <div className={`flex-shrink-0 w-screen ${sidebarOpen ? 'sm:max-w-xs max-w-screen-sm' : 'max-w-0'} transition-all overflow-hidden`}>
                 <div className="w-screen sm:max-w-xs max-w-screen-sm">
                     Woah this is a sidebar
                 </div>
