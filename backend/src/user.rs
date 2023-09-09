6c7128c08338714f002bfd6e1cd14498e7fc5744
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-09-08 22:14:46 -0700

    modified auth stuff

--- backend/src/user.rs
+++ backend/src/user.rs
@@ -1,5 +1,6 @@
 #[derive(Debug)]
-pub struct User {
+//can be used as request guard to properly authenticate users 
+pub struct User { 
     pub username: String,
     password: String,
 }
@@ -12,19 +13,19 @@ use rocket_contrib::uuid::Uuid;
 use rocket::http::Status;
 use rocket::tokio::io::AsyncReadExt;
 
-impl<'r> FromData<'r> for AdminUser {
+impl<'r> FromData<'r> for User {
     type Error = std::io::Error;
 
     async fn from_data(_req: &'r Request<'_>, data: Data<'r>) -> rocket::data::Outcome<'r, Self> {
-        let mut cookie = String::new();
-        if let Err(e) = data.open().take(256).read_to_string(&mut cookie).await {
+        let mut auth_vals = String::new();
+        if let Err(e) = data.open().take(256).read_to_string(&mut auth_vals).await {
             return Outcome::Failure((Status::InternalServerError, e));
         }
 
         // get user data from session by parsing some json token
 
 
-        if let Ok(user) = serde_json::from_str::<AdminUser>(&cookie) {
+        if let Ok(user) = serde_json::from_str::<User>(&auth_vals) {
             Outcome::Success(user)
         } else {
             Outcome::Failure((Status::Unauthorized, ()))
