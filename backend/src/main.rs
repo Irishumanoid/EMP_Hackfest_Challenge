db900f402f43db1eedd8f13f58c0ca431a35d6da
Chistopher Huntwork <christopher@huntwork.net>
2023-09-09 12:22:52 -0700

    added get_posts endpoint (it will panic tho)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,5 @@
-use rocket::serde::json::Json;
-use serde::Deserialize;
+use rocket::{serde::json::Json, http::ContentType};
+use serde::{Deserialize, Serialize};
 
 mod sightings;
 mod macros;
@@ -13,7 +13,7 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct UserPost {
     pub display_name: String,
     pub description: String,
@@ -26,6 +26,39 @@ pub struct UserPost {
 #[post("/backend/post_post", data = "<post>")]
 fn post_post(post: Json<UserPost>) {
     //Miles here
+}
+
+#[derive(Deserialize)]
+struct GetPostFilters {
+    location: [f32; 2],
+    tags: Vec<String>,
+    price_range: i32,
+}
+
+#[derive(Serialize)]
+struct APIResponse<T> {
+    success: bool,
+    error: Option<String>,
+    data: Option<T>,
+}
+
+pub fn error_string(message: String) -> String {
+    return format!("{{\"success\":true, \"error\": \"{message}\"}}");
+}
+
+pub fn sort_ports() -> Vec<UserPost> {
+    todo!();
+}
+
+#[post["/backend/get_posts", data = "<filters>"]]
+fn get_posts(filters: Json<GetPostFilters>) -> (ContentType, String) {
+    let posts = sort_ports();
+
+    let serialized_posts = serde_json::to_string(&APIResponse {success: true, error: None, data: Some(posts)});
+    return match serialized_posts {
+        Ok(posts_string) => (ContentType::JSON, posts_string),
+        Err(e) => {println!("{e}"); return (ContentType::JSON, error_string("something was wrong on the server side".to_string()));}
+    }
 }
 
 #[launch]
